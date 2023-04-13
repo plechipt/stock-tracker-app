2cfdbab7563ff08efaab78af0f96b842d7c7edf1
Jakub Plecháč <plechac.k@gmail.com>
2023-04-13 17:16:19 +0200

    added meta for tag for accessing http api

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_KEY = process.env.REACT_APP_API_KEY;
 const API_KEY2 = process.env.REACT_APP_API_KEY2;
 
-const BASE_URL = "http://api.marketstack.com/v1";
+const BASE_URL = "//api.marketstack.com/v1";
 const STOCK_URL = `${BASE_URL}/tickers?access_key=${API_KEY}`;
 const DESCRIPTION_URL = "https://financialmodelingprep.com/api/v3/profile/";
 
